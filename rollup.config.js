0dab6740aa39cceff07c1e5c03e7b4e66d6cf131
nju33 <nju33@nmba.local>
2016-03-27 00:53:14 +0900

    Tidy

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,15 @@
+const banner = `
+/*!
+ * Copyright 2016, nju33
+ * Released under the MIT License
+ * https://github.com/totora0155/tsu.js
+ */
+`;
+
 export default {
+  banner: banner.trim(),
   entry: 'lib/tsu.js',
   format: 'umd',
-  dest: 'tsu.js',
+  dest: 'dist/tsu.js',
   moduleName: 'Tsu',
 };
