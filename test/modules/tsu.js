597f4cabe9000092214cc07aaef4b02ec5610fcd
nju33 <nju33@nmba.local>
2016-04-06 01:44:19 +0900

    Write e2e

--- test/modules/tsu.js
+++ test/modules/tsu.js
@@ -1,8 +1,29 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Tsu.js' : function (browser) {
     browser
-      .url('http://www.google.com')
-      .pause(3000)
+      .url('http://localhost:3333/test/fixtures/')
+        .click('#success')
+        .pause(100)
+          .assert.visible('.tsu__box')
+          .assert.visible('.tsu__success')
+          .assert.containsText('.tsu__label', 'success')
+          .assert.containsText('.tsu__message', 'Good')
+
+      .refresh()
+        .click('#danger')
+        .pause(100)
+          .assert.visible('.tsu__danger')
+          .assert.containsText('.tsu__label', 'danger')
+          .assert.containsText('.tsu__message', 'Woops')
+
+      .refresh()
+        .click('#success')
+        .click('#success')
+        .click('#success')
+        .click('#success')
+        .pause(400)
+          .assert.elementNotPresent('.tsu__success:nth-child(4)')
+
       .end();
   }
 };
