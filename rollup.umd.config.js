bee8cd09a3c6a4a90bded6809246eebcaacb0a39
nju33 <nju33@nmba.local>
2016-04-06 22:30:42 +0900

    Rename & Fix env

--- rollup.umd.config.js
+++ rollup.umd.config.js
@@ -1,4 +1,19 @@
+import babel from 'rollup-plugin-babel';
+const banner = `
+/*!
+ * Copyright 2016, nju33
+ * Released under the MIT License
+ * https://github.com/totora0155/tsu.js
+ */
+`.trim();
+
 export default {
+  banner,
   entry: 'lib/tsu.js',
-  dest: 'es/tsu.js'
+  format: 'umd',
+  dest: 'dist/tsu.js',
+  moduleName: 'Tsu',
+  plugins: [
+    babel()
+  ]
 };
